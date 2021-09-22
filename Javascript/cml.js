6e4357f97bb80cbfeafe3498eb3a1bfdd29015e5
stephania <stephaniadorce@gmail.com>
2021-09-22 07:55:54 -0700

    2em partir du project

--- Javascript/cml.js
+++ Javascript/cml.js
@@ -38,3 +38,68 @@ var canva=document.getElementById("can");
 		context.fillStyle="#FFCC66";
 		context.closePath();
 		context.fill()
+
+		context.beginPath();
+		context.moveTo(200,230);
+		context.lineTo(250,280);
+		context.lineTo(150,280);
+		context.lineTo(200,230);
+		context.stroke();
+		context.fillStyle="#99FFFF";
+		context.closePath();
+		context.fill();
+
+
+		context.beginPath();
+		context.moveTo(180,250);
+		context.lineTo(220,250);
+		context.stroke();
+		context.fillStyle="yellow";
+		context.closePath();
+		context.fill();
+
+
+		context.beginPath();
+		context.moveTo(200,230);
+		context.lineTo(200,280);
+		context.stroke();
+		context.fillStyle="#99FFFF";
+		context.closePath();
+		context.fill();
+
+
+
+
+
+		context.beginPath();
+		context.rect(120,320,30,60);
+		context.stroke();
+		context.fillStyle="#99FFFF";
+		context.closePath();
+		context.fill();
+
+
+		context.beginPath();
+		context.rect(250,320,30,60);
+		context.stroke();
+		context.fillStyle="#99FFFF";
+		context.closePath();
+		context.fill();
+
+		
+
+		context.beginPath();
+		context.rect(170,320,60,60);
+		context.stroke();
+		context.fillStyle="#99FFFF";
+		context.closePath();
+		context.fill();
+
+
+		context.beginPath();
+		context.moveTo(200,320);
+		context.lineTo(200,380);
+		context.stroke();
+		context.fillStyle="#fff";
+		context.closePath();
+		context.fill();
